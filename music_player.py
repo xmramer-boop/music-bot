62f59f8b9ca8744f6f412fc44e50d9aaed409033
xmramer-boop <xmramer@gmail.com>
2026-06-23 15:45:56 +0400

    Update music_player.py

--- music_player.py
+++ music_player.py
@@ -3,7 +3,7 @@ from pytgcalls.types.input_stream import AudioPiped
 from pyrogram import Client
 
 api_id = 12688186
-api_hash = "YOUR_HASH"
+api_hash = "0cdd3e314b5a5487d2c99bbdc7afd450"
 
 user = Client("session", api_id=api_id, api_hash=api_hash)
 
