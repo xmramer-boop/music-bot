1f7a7d3606faa1321cf9ce33e878f618119cb2e5
xmramer-boop <xmramer@gmail.com>
2026-06-23 12:30:15 +0400

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -1,16 +1,28 @@
 from telethon import TelegramClient, events
+import os
 
 api_id = 12688186
 api_hash = "0cdd3e314b5a5487d2c99bbdc7afd450"
 bot_token = "8576876988:AAHPT7WAzo_I2N0-RLGZ840GBX91qfvv0lI"
 
-client = TelegramClient("bot", api_id, api_hash).start(
-    bot_token=bot_token
-)
+OWNER_ID = 7803165903  # آیدی عددی تلگرام خودت
+
+client = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
+
+os.makedirs("music", exist_ok=True)
 
 @client.on(events.NewMessage(pattern="/start"))
 async def start(event):
     await event.reply("ربات موزیک روشنه ✅")
+
+@client.on(events.NewMessage)
+async def save_music(event):
+    if event.sender_id != OWNER_ID:
+        return
+
+    if event.audio:
+        file_path = await event.download_media(file="music/")
+        await event.reply(f"فایل ذخیره شد ✅\n{file_path}")
 
 print("Bot is running...")
 client.run_until_disconnected()
