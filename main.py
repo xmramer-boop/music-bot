1ea28bc55f1f43778d9c1c873668dad0a149d9b8
xmramer-boop <xmramer@gmail.com>
2026-06-23 16:30:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
 from pytgcalls.types.input_stream import AudioPiped
 
-api_id = 123456
-api_hash = "API_HASH"
-bot_token = "BOT_TOKEN"
+api_id = 12688186
+api_hash = "0cdd3e314b5a5487d2c99bbdc7afd450"
+bot_token = "8576876988:AAHPT7WAzo_I2N0-RLGZ840GBX91qfvv0lI"
 
 app = Client("musicbot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 vc = PyTgCalls(app)
